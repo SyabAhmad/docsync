94bc9d4f23bd78b077a00727f665b9f1fe557c9d
SyabAhmad <syabblogger@gmail.com>
2026-06-27 13:07:19 +0300

    Add sample calculator

---
+++ sample_project/calc.py
@@ -0,0 +1,10 @@
+﻿def hello(name: str) -> str:
+    """Say hello."""
+    return f"Hello {name}"
+
+class Calculator:
+    """A simple calculator."""
+
+    def multiply(self, x: float, y: float) -> float:
+        """Multiply two numbers."""
+        return x * y
